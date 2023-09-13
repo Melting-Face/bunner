8384ba5d04722a5c789839d060fa41579c08e5bd
Jinsu Kim <jinsu.kim@tridge.com>
2023-09-13 17:05:26 +0900

    add lodash

--- test/price/gida.test.ts
+++ test/price/gida.test.ts
@@ -5,6 +5,7 @@ import {
   test,
 } from 'bun:test';
 import { load } from 'cheerio';
+import { range } from 'lodash';
 import { insert } from 'sql-bricks';
 
 import request from '../lib/request';
@@ -24,10 +25,17 @@ test('produce', async () => {
   for (const url of urls) {
     const response = await request(url);
     const $ = load(response);
-    $('script[type="text/javascript"]:not([src])').each((i, script) => {
+    $('script[type="text/javascript"]:not([src])').each((_i, script) => {
       const scriptContent = $(script).text().trim();
-      if (scriptContent.includes('ButtonEvents')) {
-        scriptContent.split('\n');
+      if (!scriptContent.includes('ButtonEvents')) {
+        return true;
+      }
+
+      const scriptRows = scriptContent.split('\n');
+      for (const row of scriptRows) {
+        if (row.includes('obj.tVal')) {
+          logger.info(row);
+        }
       }
     });
     await delay(1000);
