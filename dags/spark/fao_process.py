b97bed63254f0ceab91e0c7cd0f245ba1984b1b0
Jinsu Kim <jinsu.kim@tridge.com>
2023-09-19 16:12:47 +0900

    add process

--- dags/spark/fao_process.py
+++ dags/spark/fao_process.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from airflow.decorators import dag, task
 from constants import VERBOSE_COUNTRY_MAP
-from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql import types as T
 
@@ -10,91 +10,139 @@ from pyspark.sql import types as T
 @dag(start_date=datetime(2023, 9, 18), schedule=None)
 def fao_process():
     @task
-    def fao_price():
-        spark: SparkSession = SparkSession.builder.getOrCreate()  # noqa
-        df = spark.read.csv(
-            path='./Prices_E_All_Data_(Normalized).csv',
-            header=True,
-        )
-        df = df.where(
-            (F.col("Element") == "Producer Price (USD/tonne)")
-            & (F.col("Months") == "Annual value")
-            & (F.col("Unit") == "USD")
-        )
+    def fao_aggregate():
+        spark: SparkSession = SparkSession.builder.getOrCreate()
 
-        # filter period
-        df = df.withColumn("period", F.col("Year").cast(T.IntegerType()))
-        df = df.where(F.col("period").isNotNull())
+        # columns:
+        # Element
+        # Months,
+        # Unit
+        # Year(period)
+        # Value(price)
+        # Area(producer)
+        # Item Code(code)
+        def fao_price() -> DataFrame:
+            print("fao_price")
+            df = spark.read.csv(
+                path='./Prices_E_All_Data_(Normalized).csv',
+                header=True,
+            )
+            df = df.where(
+                (F.col("Element") == "Producer Price (USD/tonne)")
+                & (F.col("Months") == "Annual value")
+                & (F.col("Unit") == "USD")
+            )
 
-        # filter price
-        df = df.withColumn("price", F.col("Value").cast(T.FloatType()))
-        df = df.where(F.col("price").isNotNull())
+            # filter period
+            df = df.withColumn("period", F.col("Year").cast(T.IntegerType()))
+            df = df.where(F.col("period").isNotNull())
 
-        # convert area
-        area_map_list = [(key, value) for key, value in VERBOSE_COUNTRY_MAP.items()]
-        area_df = spark.createDataFrame(area_map_list, ["Area", "producer"])
-        df = df.join(area_df, ["Area"])
+            # filter price
+            df = df.withColumn("price", F.col("Value").cast(T.FloatType()))
+            df = df.where(F.col("price").isNotNull())
 
-        # normalize code
-        df = df.withColumn(
-            "code",
-            F.lpad(F.col("Item Code").cast(T.StringType()), 4, "0")
-        )
+            # convert area
+            area_map_list = [(key, value) for key, value in VERBOSE_COUNTRY_MAP.items()]
+            area_df = spark.createDataFrame(area_map_list, ["Area", "producer"])
+            df = df.join(area_df, ["Area"])
 
-        # select
-        df = df.select(["period", "producer", "code", "price"])
+            # normalize code
+            df = df.withColumn(
+                "code",
+                F.lpad(F.col("Item Code").cast(T.StringType()), 4, "0")
+            )
 
-        # result
-        df.show()
-        df.printSchema()
-        print(f"total count: {df.count()}")
+            # select
+            df = df.select(["period", "producer", "code", "price"])
+
+            # result
+            df.show()
+            df.printSchema()
+            print(f"total count: {df.count()}")
+            return df
+
+        # columns:
+        # Element
+        # Months,
+        # Unit
+        # Year(period)
+        # Value(weight)
+        # Area(producer)
+        # Item Code(code)
+        # item
+        def fao_data():
+            print("fao_data")
+            df = spark.read.csv(
+                path='./Production_Crops_Livestock_E_All_Data_(Normalized).csv',
+                header=True,
+            )
+
+            # filter Elements
+            df = df.where(
+                (F.col("Element") == "Production")
+                & (F.col("Unit") == "tonnes")
+            )
+
+            # filter period
+            df = df.withColumn("period", F.col("Year").cast(T.IntegerType()))
+            df = df.where(F.col("period").isNotNull())
+
+            # filter weight
+            df = df.withColumn("weight", F.col("Value").cast(T.LongType()))
+            df = df.where(F.col("weight").isNotNull())
+
+            # convert area
+            area_map_list = [(key, value) for key, value in VERBOSE_COUNTRY_MAP.items()]
+            area_df = spark.createDataFrame(area_map_list, ["Area", "producer"])
+            df = df.join(area_df, ["Area"])
+
+            # normalize code
+            df = df.withColumn(
+                "code",
+                F.lpad(F.col("Item Code").cast(T.StringType()), 4, "0")
+            )
+
+            # select
+            df = df.select(["period", "producer", "code", "weight", "item"])
+
+            # result
+            df.show()
+            df.printSchema()
+            print(f"total count: {df.count()}")
+            return df
+
+        def aggregate_production_code(df: DataFrame):
+            print("aggregate_production_code")
+            df = df.select("code", F.col("Item").alias("name")).distinct()
+
+            # result
+            df.show()
+            df.printSchema()
+            print(f"total count: {df.count()}")
+            return df
+
+        def aggregate_production_data(
+            data_df: DataFrame,
+            price_df: DataFrame,
+        ) -> DataFrame:
+            print("aggregate_production_data")
+            data_df = data_df.drop("item")
+            df = data_df.join(price_df, on=["period", "producer", "code"], how="outer")
+
+            # result
+            df.show()
+            df.printSchema()
+            print(f"total count: {df.count()}")
+            return df
+
+
+        price_df = fao_price()
+        data_df = fao_data()
+        code_df = aggregate_production_code(data_df)
+        data_df = aggregate_production_data(data_df, price_df)
 
         spark.stop()
 
-    @task
-    def fao_production():
-        spark: SparkSession = SparkSession.builder.getOrCreate()  # noqa
-        df = spark.read.csv(
-            path='./Production_Crops_Livestock_E_All_Data_(Normalized).csv',
-            header=True,
-        )
-
-        # filter Elements
-        df = df.where(
-            (F.col("Element") == "Production")
-            & (F.col("Unit") == "tonnes")
-        )
-
-        # filter period
-        df = df.withColumn("period", F.col("Year").cast(T.IntegerType()))
-        df = df.where(F.col("period").isNotNull())
-
-        # filter weight
-        df = df.withColumn("weight", F.col("Value").cast(T.LongType()))
-        df = df.where(F.col("weight").isNotNull())
-
-        # convert area
-        area_map_list = [(key, value) for key, value in VERBOSE_COUNTRY_MAP.items()]
-        area_df = spark.createDataFrame(area_map_list, ["Area", "producer"])
-        df = df.join(area_df, ["Area"])
-
-        # normalize code
-        df = df.withColumn(
-            "code",
-            F.lpad(F.col("Item Code").cast(T.StringType()), 4, "0")
-        )
-
-        # select
-        df = df.select(["period", "producer", "code", "weight", "item"])
-
-        # result
-        df.show()
-        df.printSchema()
-        print(f"total count: {df.count()}")
-
-        spark.stop()
-
-    fao_production()
-    fao_price()
+    fao_aggregate()
 
 fao_process()
