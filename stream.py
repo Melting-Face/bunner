49bed9b592885fa2d8d157a29adfed83c0d25462
Jinsu Kim <jinsu.kim@tridge.com>
2023-09-22 18:25:41 +0900

    add graph

---
+++ stream.py
@@ -0,0 +1,135 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+from sklearn.impute import KNNImputer
+
+WINDOW_SIZE = 21
+WEEK_SPAN = 52
+
+def write_text():
+    def impute_price(df):
+        df['date_category'] = df['date'].astype('category').cat.codes
+        imputer = KNNImputer(n_neighbors=2)
+        imputed_df = pd.DataFrame(
+            imputer.fit_transform(df[['date_category', 'price_avg']]),
+            columns=['date_category', 'price_avg_imputed'],
+        )
+        df: pd.DataFrame = df.merge(
+            imputed_df,
+            left_on=['date_category'],
+            right_on=['date_category'],
+            how='left',
+        )
+        df = df.drop('date_category', axis=1)
+        return df
+
+    def get_price_min_max_scale(series: pd.Series) -> pd.Series:
+        _max = series.max()
+        _min = series.min()
+        scaled_series = (series - _min) / (_max - _min)
+        return scaled_series
+
+    def get_price_min_max_adjusted_scale(series: pd.Series) -> pd.Series:
+        _max = series.max() * 0.9
+        _min = series.min() * 1.1
+        scaled_series = (series - _min) / (_max - _min)
+        return scaled_series
+
+    def calculate_band(series: pd.Series, sigma: float = 3.5):
+        i, start, window_size = 0, 0, WINDOW_SIZE
+
+        one_month = np.timedelta64(1, 'm')
+        upper_band = []
+        lower_band = []
+
+        while i < len(series) - window_size + 1:
+            if (series.index[i + window_size - 1] - series.index[start]) / one_month > 3: # noqa
+                start += 1
+            window = series[start : i + window_size]
+            upper = sigma * window.std() + window.mean()
+            lower = -sigma * window.std() + window.mean()
+
+            upper_band.append(upper)
+            lower_band.append(lower)
+            i += 1
+
+        upper_band = pd.Series(upper_band, index=series.index[window_size - 1 :])
+        lower_band = pd.Series(lower_band, index=series.index[window_size - 1 :])
+        if len(series) < window_size:
+            empty_series = pd.Series([np.nan] * len(series), index=series.index)
+        else:
+            empty_series = pd.Series(
+                [np.nan] * (window_size - 1), index=series.index[: window_size - 1]
+            )
+
+        upper_band = pd.concat([empty_series, upper_band])
+        lower_band = pd.concat([empty_series, lower_band])
+        return upper_band, lower_band
+
+    def scale_price(df: pd.DataFrame):
+        df['price_avg'] = df['price_avg'].astype(float)
+        df['price_avg_imputed'] = df['price_avg_imputed'].astype(float)
+
+        df['price_avg_scaled'] = get_price_min_max_scale(df['price_avg'])
+        df['price_avg_imputed_scaled'] = get_price_min_max_scale(df['price_avg_imputed']) # noqa
+
+        df['price_avg_adjusted_scaled'] = get_price_min_max_adjusted_scale(df['price_avg']) # noqa
+        df['price_avg_imputed_adjusted_scaled'] = get_price_min_max_adjusted_scale(df['price_avg_imputed']) # noqa
+
+        return df
+
+    def get_band(df: pd.DataFrame):
+        df = df.set_index('date')
+        df['upper_band'], df['lower_band'] = calculate_band(df['price_avg_imputed_adjusted_scaled']) # noqa
+        return df
+
+    entry_id = st.session_state['entry_id']
+    df = pd.read_parquet(f"whitelist_price/{entry_id}.parquet")
+    df = impute_price(df)
+    df = scale_price(df)
+    df = get_band(df)
+    df = df.reset_index()
+    df["date"] = df["date"].dt.strftime("%Y-%m-%d")
+    st.line_chart(
+        df,
+        x = "date",
+        y = ["lower_band", "price_avg_imputed_scaled", "upper_band"],
+    )
+
+option = st.selectbox(
+    key='entry_id',
+    label="Select entry_id?",
+    options=(
+        50898759,
+        51053464,
+        57082875,
+        103642448,
+        105365174,
+        106929750,
+        109993961,
+        109995061,
+        109995120,
+        111561504,
+        81039347,
+        85667518,
+        100118571,
+        100164545,
+        103386814,
+        104032939,
+        109994849,
+        110122911,
+        110123188,
+        133133919,
+        40933750,
+        40936259,
+        40936745,
+        40937436,
+        40939113,
+        40940366,
+        40940748,
+        40945912,
+        41030389,
+        41046892,
+    ),
+    on_change=write_text
+)
