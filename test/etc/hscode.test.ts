6d191f4a25703a66c0ca9273cfe0fc326c7b77d1
Jinsu Kim <jinsu.kim@tridge.com>
2023-10-17 19:09:26 +0900

    add test for hscode

--- test/etc/hscode.test.ts
+++ test/etc/hscode.test.ts
@@ -1,16 +1,31 @@
 import fs from 'fs/promises';
 
-import { test } from 'bun:test';
+import {
+  beforeAll,
+  test,
+} from 'bun:test';
+import { range } from 'lodash';
 
 import { load } from 'cheerio';
 import pl from 'nodejs-polars';
-import xlsx from 'xlsx';
+import xlsx, { Sheet } from 'xlsx';
 
 import request from '../lib/request';
-import { logger } from '../lib/utils';
+import {
+  delay,
+  logger,
+} from '../lib/utils';
+
+let response: any;
+const baseUrl = 'https://unstats.un.org';
+const filenames: Array<string> = [];
+const urls: Array<string> = [];
+
+beforeAll(async () => {
+  response = await request(`${baseUrl}/unsd/classifications/Econ`);
+});
 
 test('Get urls', async () => {
-  const response = await request('https://unstats.un.org/unsd/classifications/Econ');
   const $ = load(response);
   let index = 0;
   $('div#corresp-hs .panel-heading').each((i, el) => {
@@ -21,8 +36,58 @@ test('Get urls', async () => {
     }
   });
 
-  $('div#corresp-hs .panel-body').eq(index).find('a').each((i, a) => {
+  $('div#corresp-hs .panel-body').eq(index).find('a').each((_i, a) => {
     const href = $(a).attr('href');
-    logger.info(href);
+    let url = `${baseUrl}${href}`;
+    if (url === decodeURI(url)) {
+      url = encodeURI(url);
+    }
+    urls.push(url);
   });
 });
+
+test('Get files', async () => {
+  for (const url of urls) {
+    const filename = url.split('/').pop() || '';
+    filenames.push(filename);
+    const isExisted = await fs.exists(`xlsx/${filename}`);
+    if (!isExisted) {
+      const buffer = await request({
+        url,
+        type: 'buffer',
+      });
+      await fs.writeFile(`xlsx/${filename}`, buffer);
+      await delay(700);
+    }
+  }
+}, 60000);
+
+test('Read files', async () => {
+  for (const filename of filenames) {
+    const workbook = xlsx.readFile(`xlsx/${filename}`);
+    const sheetNames = workbook.SheetNames;
+    const sheetName = sheetNames.find((el) => el.includes('Correlation')) || '';
+    const sheet: Sheet = workbook.Sheets[sheetName];
+    logger.info('=================================');
+    logger.info(filename);
+    logger.info(sheetName);
+    logger.info(sheet.A1?.v);
+    logger.info(sheet.B1?.v);
+    let csv = xlsx.utils.sheet_to_csv(sheet);
+    const csvRows = csv.split('\n') || [];
+    if (sheet.A1?.v === 'Between') {
+      csvRows.shift();
+    }
+    if (sheet.B1?.v?.includes('Correlation')) {
+      for (const _i in range(6)) {
+        csvRows.shift();
+      }
+    }
+
+    logger.info(csvRows[0]);
+    csv = csvRows.join('\n');
+    const df = pl.readCSV(csv);
+    df.writeCSV(`csv/${filename}.csv`);
+    logger.info('=================================');
+  }
+}, 60000);
