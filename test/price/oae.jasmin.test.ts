8870684727d6470082440a82aac4197abe35e07d
Jinsu Kim <jinsu.kim@tridge.com>
2023-11-23 18:40:53 +0900

    add oae test

---
+++ test/price/oae.jasmin.test.ts
@@ -0,0 +1,48 @@
+import {
+  expect,
+  test,
+} from 'bun:test';
+import { load } from 'cheerio';
+import moment from 'moment';
+
+import {
+  delay,
+  logger,
+  request,
+} from '../lib/utils';
+
+const date = '2023-09-01';
+const THAI_SOLAR_CALENDER = 543;
+
+const htmls: Array<string> = [];
+
+test('produce', async () => {
+  const workDate = moment(date).add(THAI_SOLAR_CALENDER, 'year');
+  let response = await request('https://www.oae.go.th/view/1/%E0%B8%A3%E0%B8%B2%E0%B8%84%E0%B8%B2%E0%B8%AA%E0%B8%B4%E0%B8%99%E0%B8%84%E0%B9%89%E0%B8%B2%E0%B9%80%E0%B8%81%E0%B8%A9%E0%B8%95%E0%B8%A3/TH-TH');
+  expect(response).toBeTruthy();
+  let $ = load(response);
+  let url = $('a[title*="ข้าวหอมมะลิ 105"],a[title*="ข้าวเปลือกเจ้าหอมมะลิ 105"]').attr('href') || '';
+  let href;
+  let pageDate = '';
+  do {
+    response = await request(url);
+    $ = load(response);
+    pageDate = $('.post_meta').text().trim();
+    if (moment(pageDate, 'DD-MMMM-YYYY', 'th').isSameOrBefore(workDate)) {
+      break;
+    }
+    logger.info(url);
+    htmls.push(response);
+    logger.info(pageDate);
+    href = $('.post_desc a').attr('href');
+    expect(href).toBeTruthy();
+    url = `https://www.oae.go.th${href}`;
+    await delay(1000);
+  } while (pageDate && href);
+}, 90000);
+
+test('consume', async () => {
+  for (const html of htmls) {
+    const $ = load(html);
+  }
+});
